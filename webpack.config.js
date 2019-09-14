879e23ced70c0eecc901afc9ec1d9940c58ab988
swwn <pakulas@gmail.com>
2019-09-14 11:43:24 +0200

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,59 @@
 const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: "./src/app.js",
-    // entry: "./src/playground/hoc.js",
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            }
-        ]
-    },
-    mode: "development",
-    devtool: "cheap-module-eval-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true
-    }
+module.exports = env => {
+    const isProduction = env === "production";
+
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: "bundle.js"
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css',
+                ignoreOrder: false,
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                hmr: isProduction === 'development',
+                            },
+                        },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                    ],
+                },
+            ]
+        },
+        mode: "development",
+        devtool: isProduction ? "source-map" : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true
+        }
+    };
 };
